530c843bd30ba9f83ad966b20349f49f2f0073ae
NikitaOrehov <145605952+NikitaOrehov@users.noreply.github.com>
2025-04-18 23:52:51 +0300

    change include

--- main.cpp
+++ main.cpp
@@ -1,10 +1,4 @@
 #include <iostream>
-#include "TableTestKit.h"
-#include "ScanTable.h"
-#include <random> // c++11
-#include <algorithm> // c++11
-
-#include <iostream>
 #include <string>
 #include <vector>
 #include "ScanTable.h"
